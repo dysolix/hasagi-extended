4be5931d6d89f69a0d63138e2e77042a5fdb9ce7
dysolix <contact@dysolix.dev>
2023-11-24 02:50:32 +0100

    sync

--- src/classes/data-collection.ts
+++
@@ -1,7 +0,0 @@
-export default abstract class AbstractDataCollection<T> {
-    constructor(public readonly entries: T[]) { }
-
-    toJSON() {
-        return this.entries;
-    }
-}
--- src/constants.ts
+++ src/constants.ts
@@ -4,9 +4,4 @@ export const NORMAL_DRAFT_QUEUE_ID = 400;
 export const NORMAL_BLIND_QUEUE_ID = 430;
 export const ARAM_QUEUE_ID = 450;
 export const CLASH_QUEUE_ID = 700;
-
 export const RANKED_QUEUE_IDS = [RANKED_SOLO_QUEUE_ID, RANKED_FLEX_QUEUE_ID] as const;
-
-/* Generated on 2023-10-27T14:32:55.137Z */
-export const SERVER_REGIONS = ["br", "eune", "euw", "garena", "jp", "kr", "lan", "las", "na", "oce", "pbe", "ph", "ru", "sg", "tencent", "th", "tr", "tw", "vn"] as const;
-export const LANGUAGE_CODES = ["en_US", "cs_CZ", "de_DE", "el_GR", "en_AU", "en_GB", "en_PH", "en_SG", "es_AR", "es_ES", "es_MX", "fr_FR", "hu_HU", "it_IT", "ja_JP", "ko_KR", "pl_PL", "pt_BR", "ro_RO", "ru_RU", "th_TH", "tr_TR", "vi_VN", "zh_CN", "zh_MY", "zh_TW"] as const;
